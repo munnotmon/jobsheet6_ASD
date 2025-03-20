457929bd7b3660f2ae065220422a92435e6e6a32
maulidaprina <mldaprna@gmail.com>
2025-03-20 15:29:30 +0700

    insertion sort

--- SortingMain12.java
+++ SortingMain12.java
@@ -3,6 +3,7 @@ public class SortingMain12 {
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
         int b[] = {30, 20, 2, 8, 14};
+        int c[] = {30, 10, 4, 9, 3};
 
         Sorting12 dataurut1 = new Sorting12(a, a.length);
         
@@ -19,6 +20,14 @@ public class SortingMain12 {
         dataurut2.SelectionSort();
         System.out.println("\nData sudah diurutkan dengan SELECTION SORT (ASC)");
         dataurut2.tampil();
+
+        Sorting12 dataurut3 = new Sorting12(c, c.length);
+        
+        System.out.println("\nData awal 3");
+        dataurut3.tampil();
+        dataurut3.insertionSort();
+        System.out.println("\nData sudah diurutkan dengan INSERTION SORT (ASC)");
+        dataurut3.tampil();
     }    
 }
 
