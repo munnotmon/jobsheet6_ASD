9ee101e238f0958e232ffd70ecd1514e9fcea917
maulidaprina <mldaprna@gmail.com>
2025-03-20 13:55:03 +0700

    Sorting – Selection Sort

--- SortingMain12.java
+++ SortingMain12.java
@@ -2,7 +2,8 @@ public class SortingMain12 {
     
     public static void main(String[] args) {
         int a[] = {20, 10, 2, 7, 12};
-    
+        int b[] = {30, 20, 2, 8, 14};
+
         Sorting12 dataurut1 = new Sorting12(a, a.length);
         
         System.out.println("\nData awal 1");
@@ -10,6 +11,14 @@ public class SortingMain12 {
         dataurut1.bubbleSort();
         System.out.println("\nData sudah diurutkan dengan BUBBLE SORT (ASC)");
         dataurut1.tampil();
+
+        Sorting12 dataurut2 = new Sorting12(b, b.length);
+        
+        System.out.println("\nData awal 2");
+        dataurut2.tampil();
+        dataurut2.SelectionSort();
+        System.out.println("\nData sudah diurutkan dengan SELECTION SORT (ASC)");
+        dataurut2.tampil();
     }    
 }
 
